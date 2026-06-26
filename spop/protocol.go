71004fd559f4da93236429da5d469a3813424d77
sabban <github@sabban.eu>
2026-06-26 18:09:51 +0200

    Increase SPOE max frame size

--- spop/protocol.go
+++ spop/protocol.go
@@ -88,7 +88,7 @@ const (
 
 	// maxFrameSize represents the maximum frame size allowed by this library
 	// it also represents the maximum slice size that is allowed on stack
-	maxFrameSize = 64<<10 - 1
+	maxFrameSize = 128<<10 - 1
 )
 
 func (c *protocolClient) onHAProxyHello(f *frame) error {
